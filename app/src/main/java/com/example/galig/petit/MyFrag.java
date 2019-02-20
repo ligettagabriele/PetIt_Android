ad5786707bc96ea56b73cd6f8ff0af635bba65ac
Andrea <andrearovatti05@gmail.com>
2019-02-20 16:23:42 +0100

    Tutorial seconda parte

--- app/src/main/java/com/example/galig/petit/MyFrag.java
+++ app/src/main/java/com/example/galig/petit/MyFrag.java
@@ -13,8 +13,7 @@ public class MyFrag extends Fragment {
     }
 
     @Override
-    public View onCreateView( LayoutInflater inflater,  ViewGroup container,
-                              Bundle savedInstanceState) {
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tutorial1_layout,
                 container, false);
         return rootView;
