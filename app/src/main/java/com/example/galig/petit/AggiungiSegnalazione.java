32b0890317fe194a02f96c334c58e903ac82cdc4
Federico <f.giajlevra@itsrizzoli.it>
2019-02-20 12:58:08 +0100

    Parte java + modifiche a manifest

--- app/src/main/java/com/example/galig/petit/AggiungiSegnalazione.java
+++ app/src/main/java/com/example/galig/petit/AggiungiSegnalazione.java
@@ -2,7 +2,13 @@ package com.example.galig.petit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AggiungiSegnalazione extends Activity {
 
@@ -13,23 +19,37 @@ public class AggiungiSegnalazione extends Activity {
     private String statoMentale;
     private String note;
 
-    private EditText colorePeloET = (EditText) findViewById(R.id.colorePelo);
-    private EditText tipoPeloET = (EditText) findViewById(R.id.tipoPelo);
-    private EditText tagliaET = (EditText) findViewById(R.id.taglia);
-    private EditText statoFisicoET = (EditText) findViewById(R.id.statoFisico);
-    private EditText statoMentaleET = (EditText) findViewById(R.id.statoMentale);
-    private EditText noteET = (EditText) findViewById(R.id.infoExtra);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiungi_segnalazione);
 
-        colorePelo = colorePeloET.getText().toString();
-        tipoPelo = tipoPeloET.getText().toString();
-        taglia = tagliaET.getText().toString();
-        statoFisico = statoFisicoET.getText().toString();
-        statoMentale = statoMentaleET.getText().toString();
-        note = noteET.getText().toString();
+        final EditText colorePeloET = (EditText) findViewById(R.id.colorePelo);
+        final EditText tipoPeloET = (EditText) findViewById(R.id.tipoPelo);
+        final Spinner tagliaSP = (Spinner) findViewById(R.id.taglia);
+        final EditText statoFisicoET = (EditText) findViewById(R.id.statoFisico);
+        final EditText statoMentaleET = (EditText) findViewById(R.id.statoMentale);
+        final EditText noteET = (EditText) findViewById(R.id.infoExtra);
+        //  ImageButton posizione = (ImageButton) findViewById(R.id.bottone_posizione);
+        Button inviaSegnalazione = (Button) findViewById(R.id.immettiSegnalazione);
+
+
+        inviaSegnalazione.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                colorePelo = colorePeloET.getText().toString();
+                tipoPelo = tipoPeloET.getText().toString();
+                taglia = tagliaSP.getSelectedItem().toString();
+                statoFisico = statoFisicoET.getText().toString();
+                statoMentale = statoMentaleET.getText().toString();
+                note = noteET.getText().toString();
+
+                Toast.makeText(AggiungiSegnalazione.this, taglia, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
