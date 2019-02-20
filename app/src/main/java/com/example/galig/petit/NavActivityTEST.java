591f317f7d78fcc8025cf073f70ed973ebaadb61
Federico <f.giajlevra@itsrizzoli.it>
2019-02-20 14:25:38 +0100

    Aggiunto logo + modifcato Theme per far funzionare Navactivity

--- app/src/main/java/com/example/galig/petit/NavActivityTEST.java
+++ app/src/main/java/com/example/galig/petit/NavActivityTEST.java
@@ -1,5 +1,6 @@
 package com.example.galig.petit;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,25 +16,24 @@ public class NavActivityTEST extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
-    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId())
-            {
-                case R.id.action_add:
-                    Toast.makeText(NavActivityTEST.this, "Add", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.action_mappa:
-                    Toast.makeText(NavActivityTEST.this, "Mappa", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.action_menu:
-                    Toast.makeText(NavActivityTEST.this, "Menu", Toast.LENGTH_SHORT).show();
-                    break;
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_add:
+                        Toast.makeText(NavActivityTEST.this, "Add", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_mappa:
+                        Toast.makeText(NavActivityTEST.this, "Mappa", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_menu:
+                        Toast.makeText(NavActivityTEST.this, "Menu", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
             }
-            return true;
-        }
-    });
+        });
 
     }
 }
