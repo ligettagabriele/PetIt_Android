08a43126a3bbe2fdeb3e3625662bfb493230eed4
Federico <f.giajlevra@itsrizzoli.it>
2019-02-20 21:53:00 +0100

    ora se si carica NavActivtyTEST si apre in automatico la pagina menu al posto di una schermata bianca

--- app/src/main/java/com/example/galig/petit/NavActivityTEST.java
+++ app/src/main/java/com/example/galig/petit/NavActivityTEST.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -18,6 +19,10 @@ public class NavActivityTEST extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MenuNavFragment()).commit();
+        }
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
