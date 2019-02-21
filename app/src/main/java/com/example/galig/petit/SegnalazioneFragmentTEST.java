613acdb99d33fe65db7b70fb5bcba9532452c3a9
Federico <f.giajlevra@itsrizzoli.it>
2019-02-21 12:26:16 +0100

    Bottoni funzionali segnalazione_v2

--- app/src/main/java/com/example/galig/petit/SegnalazioneFragmentTEST.java
+++ app/src/main/java/com/example/galig/petit/SegnalazioneFragmentTEST.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,12 +31,11 @@ public class SegnalazioneFragmentTEST extends Fragment implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        final ImageButton gattoButton = (ImageButton) v.findViewById(R.id.gatto);
-        final ImageButton caneButton = (ImageButton) v.findViewById(R.id.cane);
 
         switch (v.getId()) {
             case R.id.cane:
                 Toast.makeText(getContext(), "cane", Toast.LENGTH_SHORT).show();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new SegnalazioneFragment()).commit();
                 break;
             case R.id.gatto:
                 Toast.makeText(getContext(), "gatto", Toast.LENGTH_SHORT).show();
