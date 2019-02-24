5efc682b08fc95b5a32e1c4ea6875e900273d43b
laracorona <l.corona@itsrizzoli.it>
2019-02-24 17:31:02 +0100

    SplashActivity aggiunta

---
+++ app/src/main/java/com/example/galig/petit/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.galig.petit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(1500L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+        finish();
+    }
+}
